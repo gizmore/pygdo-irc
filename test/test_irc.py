2d6000fe26fe7687ee5e82870043b4bde11a0c96
gizmore <gizmore@wechall.net>
2024-05-16 13:52:07 +0200

    'all tests pass'

--- test/test_irc.py
+++ test/test_irc.py
@@ -8,7 +8,7 @@ from gdo.base.Exceptions import GDOParamError
 from gdo.base.ModuleLoader import ModuleLoader
 from gdo.core.GDO_Server import GDO_Server
 from gdo.core.method.launch import launch
-from gdotest.TestUtil import reinstall_module, cli_plug, get_gizmore, install_module
+from gdotest.TestUtil import reinstall_module, cli_plug, web_gizmore, install_module
 
 
 class IRCPlug:
@@ -39,12 +39,12 @@ class IRCTestCase(unittest.TestCase):
         loader.init_cli()
         if IRCTestCase.IRC_SERVER is None:
             num_servers = GDO_Server.table().count_where()
-            cli_plug(get_gizmore(), f"add_server giz.org_{num_servers + 1} IRC irc://irc.giz.org:6667")
+            cli_plug(web_gizmore(), f"add_server giz.org_{num_servers + 1} IRC irc://irc.giz.org:6667")
             IRCTestCase.IRC_SERVER = GDO_Server.table().select().where("serv_connector='IRC'").order('serv_created DESC').first().exec().fetch_object()
 
     def test_01_add_irc_server(self):
         num_servers = GDO_Server.table().count_where()
-        out = cli_plug(get_gizmore(), f"add_server giz.org_{num_servers + 1} IRC irc://irc.giz.org:6667")
+        out = cli_plug(web_gizmore(), f"add_server giz.org_{num_servers + 1} IRC irc://irc.giz.org:6667")
         self.assertIn('new IRC server', out, "Cannot add IRC server")
         pattern = r'#(\d+)'
         match = re.search(pattern, out)
@@ -53,7 +53,7 @@ class IRCTestCase(unittest.TestCase):
     def test_02_add_invalid_irc_server(self):
         num_servers = GDO_Server.table().count_where()
         with self.assertRaises(GDOParamError):
-            out = cli_plug(get_gizmore(), f"add_server giz.org_{num_servers + 1} IRC irc://irc.giz.org:6668")
+            out = cli_plug(web_gizmore(), f"add_server giz.org_{num_servers + 1} IRC irc://irc.giz.org:6668")
             self.assertNotIn('new IRC server', out, "Would have added an invalid IRC server")
 
     # def test_03_connect_irc_server(self):
@@ -67,7 +67,7 @@ class IRCTestCase(unittest.TestCase):
     def test_04_help(self):
         from gdo.core.method.help import help
         server = IRCTestCase.IRC_SERVER
-        user = get_gizmore()
+        user = web_gizmore()
         out = help().env_server(server).env_user(user).gdo_execute().render_irc()
         self.assertIn('Core', out, 'IRC Help does not work.')
 
